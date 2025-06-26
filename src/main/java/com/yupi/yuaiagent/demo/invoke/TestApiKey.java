f4971327501db9ffa8a75fc0078129975db1f7bc
itcast <574471862@qq.com>
2025-06-26 13:32:55 +0800

    智能体提交

---
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,7 @@
+package com.yupi.yuaiagent.demo.invoke;
+
+public class TestApiKey {
+
+   static String API_KEY="sk-580f4b385f4a415389433752807eb0c5";
+
+}
---
+++ src/main/java/com/yupi/yuaiagent/tools/FileOperationTool.java
@@ -0,0 +1,37 @@
+package com.yupi.yuaiagent.tools;
+
+import cn.hutool.core.io.FileUtil;
+import com.yupi.yuaiagent.constant.FileConstant;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+
+import java.util.List;
+
+/**
+ * 文件操作工具类（提供文件读写功能）
+ */
+public class FileOperationTool {
+    private final String FILE_DIR= FileConstant.FILE_SAVE_DIR+"/file";
+    @Tool(description = "Read content from a file")
+    public String readFile(@ToolParam(description = "Name of a file to read") String fileName){
+        String filepath=FILE_DIR+"/"+fileName;
+        try {
+            return FileUtil.readUtf8String(filepath);
+        }catch (Exception e){
+            return "Error reading file"+e.getMessage();
+        }
+    }
+    @Tool(description = "write content from a file")
+    public String writeFile(@ToolParam(description = "Name of a file to read") String fileName,
+                            @ToolParam(description = "Content to write to the file") String content){
+        String filepath=FILE_DIR+"/"+fileName;
+        try {
+            // 创建目录
+            FileUtil.mkdir(FILE_DIR);
+            FileUtil.writeUtf8String(content, filepath);
+            return "File written successfully to: " + filepath;
+        } catch (Exception e) {
+            return "Error writing to file: " + e.getMessage();
+        }
+    }
+}
